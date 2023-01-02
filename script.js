7fee901b374d38cb1326354e28f84238d2cc997d
Loc Bui <buda@Locs-MBP.Starry-inc.net>
2023-01-02 14:23:31 -0500

    test modal functioning on first image

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+const spirited = document.querySelector('.spirited')
+const modal = document.querySelector('.spiritedModal')
+const closeButton = document.querySelector(".close-button")
+
+function showSpirited() {
+    modal.classList.toggle("show-spiritedModal")
+}
+
+function windowClick (event) {
+    if (event.target === modal) {
+        showSpirited();
+    }
+}
+
+spirited.addEventListener('click', showSpirited);
+closeButton.addEventListener('click', showSpirited);
+window.addEventListener('click', windowClick)
